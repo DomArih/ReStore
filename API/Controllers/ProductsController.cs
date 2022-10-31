b84348f70a65322f9bbfc8295ca0eb6c1bcd93a7
Dominik Arih <dominik.arih@uprise.hr>
2022-10-31 13:19:28 +0100

    End of section 8

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -1,13 +1,18 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text.Json;
 using System.Threading.Tasks;
 using API.Data;
 using API.Entities;
+using API.Extensions;
+using API.RequestHelpers;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Infrastructure;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging.Configuration;
+using Microsoft.VisualBasic;
 
 namespace API.Controllers
 {
@@ -21,9 +26,19 @@ namespace API.Controllers
         }
 
         [HttpGet]
-        public async Task<ActionResult<List<Product>>> GetProducts()
+        public async Task<ActionResult<PagedList<Product>>> GetProducts([FromQuery]ProductParams productParams)
         {
-            return await _context.Products.ToListAsync();
+            var query =  _context.Products
+                .Sort(productParams.OrderBy)
+                .Search(productParams.SearchTerm)
+                .Filter(productParams.Brands, productParams.Types)
+                .AsQueryable();
+
+            var products = await PagedList<Product>.ToPagedList(query, productParams.PageNumber, productParams.PageSize);
+
+            Response.AddPaginationHeader(products.MetaData);
+
+            return products;
         }
 
         [HttpGet("{id}")]
@@ -33,5 +48,15 @@ namespace API.Controllers
             if(product == null) return NotFound();
             return product;
         }
+
+        [HttpGet("filters")]
+        public async Task<IActionResult> GetFilters()
+        {
+            var brands = await _context.Products.Select(p => p.Brand).Distinct().ToListAsync();
+            var types = await _context.Products.Select(p => p.Type).Distinct().ToListAsync();
+
+            return Ok(new {brands, types});
+        }
+
     }
 }
--- client/src/features/catalog/Catalog.tsx
+++ client/src/features/catalog/Catalog.tsx
@@ -1,27 +1,82 @@
 import { ThemeContext } from "@emotion/react";
+import { Box, Checkbox, FormControl, FormControlLabel, FormGroup, FormLabel, Grid, Pagination, Paper, Radio, RadioGroup, TextField, Typography } from "@mui/material";
 import { useEffect, useState } from "react";
 import agent from "../../app/api/agent";
 import Agent from "../../app/api/agent";
+import AppPagination from "../../app/components/AppPagination";
+import CheckBoxButtons from "../../app/components/CheckboxButtons";
+import RadioButtonGroup from "../../app/components/RadioButtonGroup";
 import { Product } from "../../app/models/product";
 import { useAppDispatch, useAppSelector } from "../../app/store/configureStore";
-import { fetchProductsAsync, productSelectors } from "./catalogSlice";
+import { fetchFilters, fetchProductsAsync, productSelectors, setPageNumber, setProductParams } from "./catalogSlice";
 import ProductList from "./ProductList";
+import ProductSearch from "./ProductSearch";
+
+const sortOptions = [
+    {value: 'name', label: 'Alphabetical'},
+    {value: 'priceDesc', label: 'Price - High to low'},
+    {value: 'price', label: 'Price - Low to high'},
+]
 
 export default function Catalog()
 {   
     const products = useAppSelector(productSelectors.selectAll);
-    const {productsLoaded} = useAppSelector(state => state.catalog);
+    const {productsLoaded, status, filtersLoaded, brands, types, productParams, metaData} = useAppSelector(state => state.catalog);
     const dispatch = useAppDispatch();
 
 useEffect(() => {
   if(!productsLoaded) dispatch(fetchProductsAsync());
 }, [productsLoaded,dispatch])
 
-//if(status,includes('pending')) return <LoadingComponent message = 'Loading products.../>
+useEffect(() => {
+    if(!filtersLoaded) dispatch(fetchFilters());
+  }, [dispatch, filtersLoaded])
+
+if(!filtersLoaded) return <h3>Loading ...</h3>;
 
     return(
-        <>
-            <ProductList products={products} />
-        </>
+        <Grid container columnSpacing={4}>
+            <Grid item xs={3}>
+                <Paper sx={{mb:2}}>
+                    <ProductSearch />
+
+                </Paper>
+
+                <Paper sx = {{mb: 2, p:2}}>
+                    <RadioButtonGroup 
+                        selectedValue = {productParams.orderBy}
+                        options = {sortOptions}
+                        onChange= {(e) => dispatch(setProductParams({orderBy: e.targer.value}))}
+                    />
+                </Paper>
+
+                <Paper sx = {{mb: 2, p:2}}>
+                    <CheckBoxButtons 
+                        items = {brands}
+                        checked = {productParams.brands}
+                        onChange = {(items: string[]) => dispatch(setProductParams({brands: items}))}
+                    />
+                </Paper>
+
+                <Paper sx = {{mb: 2, p:2}}>
+                <CheckBoxButtons 
+                        items = {types}
+                        checked = {productParams.types}
+                        onChange = {(items: string[]) => dispatch(setProductParams({types: items}))}
+                    />
+                </Paper>
+            </Grid>
+            <Grid item xs={9}>
+                  <ProductList products={products} />
+            </Grid>
+           <Grid item xs={3} />
+           <Grid item xs={9} sx={{mb: 2}}>
+                {metaData &&
+                <AppPagination
+                    metaData = {metaData}
+                    onPageChange = {(page:number) => dispatch(setPageNumber({pageNumber: page}))}
+                />}
+           </Grid>
+        </Grid>
     )
 }
